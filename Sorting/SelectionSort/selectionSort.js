eb6b5c0420596803ddd368922fb97d398ab6351d
Krishna Vishwakarma <krishnavishwa18@gmail.com>
2025-02-09 23:12:03 +0530

    insertion, bubble, selection sort

--- Sorting/SelectionSort/selectionSort.js
+++ Sorting/SelectionSort/selectionSort.js
@@ -1,13 +1,15 @@
-let arr = [1,3,5,7,9,8,6,4,2,0];
+let arr = [1,3,5,7,9,8,6,4,2,10];
 
 const selectionSort = (arr) => {
     let len = arr.length;
     for(let i=0;i<len;i++){
-        for(let j=i+1;j<len;j++){
-            if(arr[j]<arr[i]) [arr[i], arr[j]] = [arr[j], arr[i]];
-        }
+      let minEleIndex = i;
+      for(let j=i+1;j<len;j++){
+        if(arr[j]<arr[minEleIndex]) minEleIndex = j;
+      }
+      [arr[i], arr[minEleIndex]] = [arr[minEleIndex], arr[i]];
     }
-}
+  }
 
 selectionSort(arr);
 console.log(arr);
